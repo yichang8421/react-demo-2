856ad398bba7f37dc91a1c86154088bd1fdc20cb
andy8421 <yichang8421@qq.com>
2021-08-18 23:42:41 +0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import {useState, createContext, useContext} from "react";
-
-const C = createContext(null);
+import {useState, useEffect} from "react";
 
 function App() {
     const [n, setN] = useState(0);
-    const api = {n, setN};
 
-    return (
-        <C.Provider value={api}>
-            <Child/>
-        </C.Provider>
-    );
-}
+    // componentDidMount
+    useEffect(() => {
+        console.log("只在第一次渲染时执行");
+    }, []);
 
-const Child = () => {
-    const {n, setN} = useContext(C);
+    // componentDidUpdate (Except for the first time)
+    useEffect(() => {
+        if (n > 0)
+            console.log("除第一次渲染以外，n 更新就执行");
+    }, [n]);
+
+    // componentDidUpdate
+    useEffect(() => {
+        console.log("任何变化都执行");
+    });
+
+    // componentWillUnmount
+    useEffect(() => {
+        const id = setInterval(() => {
+            console.log('carry out');
+        }, 1000)
+        return () => {
+            window.clearInterval(id);
+        }
+    }, []);
 
     return (
         <div>
-            child n: {n}
+            n: {n}
             <br/>
             <button onClick={() => {
                 setN(i => i + 1);
@@ -26,6 +39,6 @@ const Child = () => {
             </button>
         </div>
     );
-};
+}
 
 export default App;
