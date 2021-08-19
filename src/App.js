7adc732696a93c55120c4e247757888ad728e870
andy8421 <yichang8421@qq.com>
2021-08-19 21:08:05 +0800

    React.forwardRef

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
-import React, {useRef, useState} from "react";
-import ReactDOM from "react-dom";
+import React, {forwardRef, useRef} from "react";
 
 function App() {
-    const [n, setN] = useState(0);
-    const count = useRef(0);
-
-    const [, set_] = useState(null);
-
+    const buttonRef = useRef(null);
     return (
-        <div id="n">
-            n: {n}
-            <br/>
-            <button onClick={() => {
-                setN(i => i + 1);
-            }}>n+1
-            </button>
-            <hr/>
-            count.current: {count.current}
-            <br/>
-            <button onClick={() => {
-                count.current++;
-                console.log(count.current);
-                // ReactDOM.render(
-                //     <App/>
-                //     , document.getElementById('root'));
-                set_(Math.random());
-            }}>count.current+1
-            </button>
+        <div>
+            <Button ref={buttonRef}>自定义按钮</Button>
         </div>
     );
 }
+
+const Button = forwardRef(function (props, ref) {
+    console.log(props);
+    console.log(ref);
+    return (
+        <button className="child" {...props} ref={ref}></button>
+    );
+})
 
 export default App;
